85fed411c605e77ff148421a914c6b8481ae3c07
mnvijay1998 <mnvijay1998@gmail.com>
2019-05-27 20:47:16 +0530

    Update vowel.py

--- set1/vowel.py
+++ set1/vowel.py
@@ -2,3 +2,8 @@ n=input()
 l=['a','e','i','o','u','A','E','I','O','U']
 if n in l:
 	print("Vowel")
+if n not in l:
+	if n>='a' and n<='z' or n>='A' and n<='Z':
+		print("Consonant")
+	else:
+		print("invalid")
